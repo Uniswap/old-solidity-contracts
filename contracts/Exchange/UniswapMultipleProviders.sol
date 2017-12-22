e0b9e619bcd2043e41306643e8b12cf2080d138d
Hayden Adams <haydenzadams@gmail.com>
2017-12-22 01:39:11 -0500

    liquidity providers receive proportional shares of market

--- contracts/Exchange/UniswapMultipleProviders.sol
+++ contracts/Exchange/UniswapMultipleProviders.sol
@@ -2,32 +2,32 @@ pragma solidity ^0.4.18;
 
 
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
     }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
 
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
 
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
 
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
 }
 
 
@@ -53,7 +53,8 @@ contract UniswapBasic {
 
     uint256 public ethInMarket;
     uint256 public tokensInMarket;
-    uint256 public invariant = 0;
+    uint256 public invariant;
+    uint256 public feePool;
     address public tokenAddress;
     ERC20Token token;
 
@@ -61,32 +62,50 @@ contract UniswapBasic {
         require(invariant > 0);
         require(msg.value != 0 && minimumTokens != 0 && now < timeout);
         uint256 fee = msg.value.div(feeRate);
-        uint256 ethInPurchase = msg.value.sub(fee);
-        uint256 newTotalEth = ethInMarket.add(ethInPurchase);
-        uint256 newTotalTokens = invariant.div(newTotalEth);
-        uint256 purchasedTokens = tokensInMarket.sub(newTotalTokens);
+        uint256 ethSold = msg.value.sub(fee);
+        uint256 newEthInMarket = ethInMarket.add(ethSold);
+        uint256 newTokensInMarket = invariant.div(newEthInMarket);
+        uint256 purchasedTokens = tokensInMarket.sub(newTokensInMarket);
         require(purchasedTokens >= minimumTokens && purchasedTokens <= tokensInMarket);
-        ethInMarket = newTotalEth;
-        tokensInMarket = newTotalTokens;
-        TokenPurchase(msg.sender, purchasedTokens, ethInPurchase);
+        feePool = feePool.add(fee);
+        ethInMarket = newEthInMarket;
+        tokensInMarket = newTokensInMarket;
+        TokenPurchase(msg.sender, purchasedTokens, ethSold);
         token.transfer(msg.sender, purchasedTokens);
     }
 
 
-    function tokenToEth(uint256 sellQuantity, uint256 minimumEth, uint256 timeout) public {
+    function fallbackEthToTokens(address buyer, uint256 value) internal {
         require(invariant > 0);
-        require(sellQuantity !=0 && minimumEth != 0 && now < timeout);
-        uint256 fee = sellQuantity.div(feeRate);
-        uint256 tokensInPurchase = sellQuantity - fee;
-        uint256 newTotalTokens = tokensInMarket.add(tokensInPurchase);
-        uint256 newTotalEth = invariant.div(newTotalTokens);
-        uint256 purchasedEth = ethInMarket.sub(newTotalEth);
-        require(purchasedEth >= minimumEth && purchasedEth <= ethInMarket);
-        tokensInMarket = newTotalTokens;
-        ethInMarket = newTotalEth;
-        EthPurchase(msg.sender, purchasedEth, tokensInPurchase);
+        uint256 fee = value.div(feeRate);
+        uint256 ethSold = value.sub(fee);
+        uint256 newEthInMarket = ethInMarket.add(ethSold);
+        uint256 newTokensInMarket = invariant.div(newEthInMarket);
+        uint256 purchasedTokens = tokensInMarket.sub(newTokensInMarket);
+        require(purchasedTokens <= tokensInMarket.div(10)); //cannot buy more than 10% of tokens through fallback function
+        feePool = feePool.add(fee);
+        ethInMarket = newEthInMarket;
+        tokensInMarket = newTokensInMarket;
+        TokenPurchase(buyer, purchasedTokens, ethSold);
+        token.transfer(buyer, purchasedTokens);
+    }
+
+
+    function tokenToEth(uint256 tokensSold, uint256 minimumEth, uint256 timeout) public {
+        require(invariant > 0);
+        require(tokensSold !=0 && minimumEth != 0 && now < timeout);
+        uint256 newTokensInMarket = tokensInMarket.add(tokensSold);
+        uint256 newEthInMarket = invariant.div(newTokensInMarket);
+        uint256 purchasedEth = ethInMarket.sub(newEthInMarket);
+        uint256 fee = purchasedEth.div(feeRate);
+        uint256 ethToBuyer = purchasedEth.sub(fee);
+        require(ethToBuyer >= minimumEth && purchasedEth <= ethInMarket);
+        feePool = feePool.add(fee);
+        tokensInMarket = newTokensInMarket;
+        ethInMarket = newEthInMarket;
+        EthPurchase(msg.sender, purchasedEth, tokensSold);
         msg.sender.transfer(purchasedEth);
-        token.transferFrom(msg.sender, address(this), sellQuantity);
+        token.transferFrom(msg.sender, address(this), tokensSold);
     }
 }
 
@@ -94,51 +113,83 @@ contract UniswapBasic {
 contract UniswapMultipleProviders is UniswapBasic {
     using SafeMath for uint256;
 
-    event Investment(address indexed liquidityProvider, uint256 indexed investedEth, uint256 indexed investedTokens);
-    event Divestment(address indexed liquidityProvider, uint256 indexed divestedEth, uint256 indexed divestedTokens);
+    event Investment(address indexed liquidityProvider, uint256 indexed sharesPurchased);
+    event Divestment(address indexed liquidityProvider, uint256 indexed sharesSold);
 
-    mapping(address => uint256) liquidityBalance;
+    uint256 public totalShares;
+    uint256 public lastFeeDistribution;
+    mapping(address => uint256) liquidityShares;
     mapping(address => uint256) divestedEthBalance;
     mapping(address => uint256) divestedTokenBalance;
+    mapping(address => uint256) feeBalance;
+    mapping(address => bool) isInvestor;
+    address[] investors;
+
+    modifier waitingPeriod() {
+        uint lockoutEnd = lastFeeDistribution.add(1 weeks);
+        require(now > lockoutEnd);
+        _;
+    }
 
     function UniswapMultipleProviders(address _tokenAddress) public {
         tokenAddress = _tokenAddress;
         token = ERC20Token(tokenAddress);
+        lastFeeDistribution = now;
     }
 
 
-    function investLiquidity(uint256 tokenAmount) external payable {
-        require(tokenAmount != 0 || msg.value != 0);
-        require(tokenAmount >= 0);
-        uint256 newTotalEth = ethInMarket.add(msg.value);
-        uint256 newTotalTokens = tokensInMarket.add(tokenAmount);
-        uint256 newInvariant = newTotalTokens.mul(newTotalEth);
-        uint256 providedLiquidity = newInvariant.sub(invariant);
-        liquidityBalance[msg.sender] = liquidityBalance[msg.sender].add(providedLiquidity);
-        ethInMarket = newTotalEth;
-        tokensInMarket = newTotalTokens;
-        invariant = newInvariant;
-        Investment(msg.sender, msg.value, tokenAmount);
+    function() public payable {
+        require(msg.value != 0);
+        fallbackEthToTokens(msg.sender, msg.value);
+    }
+
+    // Needs work - numbers are somewhat arbitrary right now
+    function initializeExchange(uint256 tokenAmount) external payable {
+        require(invariant == 0 && totalShares == 0);
+        require(msg.value >= 1000000 && tokenAmount >= 1000000);
+        ethInMarket = msg.value;
+        tokensInMarket = tokenAmount;
+        invariant = ethInMarket.mul(tokensInMarket);
+        liquidityShares[msg.sender] = 1000;
+        totalShares = 1000;
         token.transferFrom(msg.sender, address(this), tokenAmount);
     }
 
 
-    function divestLiquidity(uint256 divestedLiquidity) external {
-        require(divestedLiquidity > 0);
-        liquidityBalance[msg.sender] = liquidityBalance[msg.sender].sub(divestedLiquidity);
-        uint256 newInvariant = invariant.sub(divestedLiquidity);
-        uint256 ethTokenRatio = ethInMarket.div(tokensInMarket);
-        uint256 newMarketTokensSquared = newInvariant.div(ethTokenRatio);
-        uint256 newMarketTokens = sqrt(newMarketTokensSquared);
-        uint256 newMarketEth = newInvariant.div(newMarketTokens);
-        uint256 divestedEth = ethInMarket.sub(newMarketEth);
-        uint256 divestedTokens = tokensInMarket.sub(newMarketTokens);
-        divestedEthBalance[msg.sender] = divestedEth;
-        divestedTokenBalance[msg.sender] = divestedTokens;
-        ethInMarket = newMarketEth;
-        tokensInMarket = newMarketTokens;
-        invariant = newInvariant;
-        Divestment(msg.sender, divestedEth, divestedTokens);
+    function investLiquidity() external payable {
+        require(invariant != 0);
+        uint256 weiPerShare = ethInMarket.div(totalShares);
+        require(msg.value >= weiPerShare);
+        uint256 sharesPurchased = msg.value.div(weiPerShare);
+        uint256 tokensPerShare = tokensInMarket.div(totalShares);
+        uint256 tokensRequired = sharesPurchased.mul(tokensPerShare);
+        liquidityShares[msg.sender] = liquidityShares[msg.sender].add(sharesPurchased);
+        totalShares = totalShares.add(sharesPurchased);
+        ethInMarket = ethInMarket.add(msg.value);
+        tokensInMarket = tokensInMarket.add(tokensRequired);
+        invariant = ethInMarket.mul(tokensInMarket);
+        if(isInvestor[msg.sender] == false) {
+            isInvestor[msg.sender] == true;
+            investors.push(msg.sender);
+        }
+        Investment(msg.sender, sharesPurchased);
+        token.transferFrom(msg.sender, address(this), tokensRequired);
+    }
+
+
+    function divestLiquidity(uint256 sharesSold) external {
+        liquidityShares[msg.sender] = liquidityShares[msg.sender].sub(sharesSold);
+        uint256 weiPerShare = ethInMarket.div(totalShares);
+        uint256 tokensPerShare = tokensInMarket.div(totalShares);
+        uint256 weiDivested = weiPerShare.mul(sharesSold);
+        uint256 tokensDivested = tokensPerShare.mul(sharesSold);
+        totalShares = totalShares.sub(sharesSold);
+        divestedEthBalance[msg.sender] = divestedEthBalance[msg.sender].add(weiDivested);
+        divestedTokenBalance[msg.sender] = divestedEthBalance[msg.sender].add(tokensDivested);
+        ethInMarket = ethInMarket.sub(weiDivested);
+        tokensInMarket = tokensInMarket.sub(tokensDivested);
+        invariant = ethInMarket.mul(tokensInMarket);
+        Divestment(msg.sender, sharesSold);
     }
 
 
@@ -158,8 +209,8 @@ contract UniswapMultipleProviders is UniswapBasic {
     }
 
 
-    function getLiquidityBalance(address provider) external view returns(uint256 balance){
-        return liquidityBalance[provider];
+    function getShares(address provider) external view returns(uint256 shares){
+        return liquidityShares[provider];
     }
 
 
@@ -169,12 +220,15 @@ contract UniswapMultipleProviders is UniswapBasic {
     }
 
 
-    function sqrt(uint256 x) internal pure returns (uint256 y) {
-        uint256 z = (x + 1) / 2;
-        y = x;
-        while (z < y) {
-            y = z;
-            z = (x / z + z) / 2;
+    // Needs to be replaced
+    // https://medium.com/@weka/dividend-bearing-tokens-on-ethereum-42d01c710657
+    function distributeFees() public waitingPeriod {
+        for (uint i = 0; i < investors.length; i++) {
+          address investor = investors[i];
+          uint256 newFees = feePool * (liquidityShares[investor] / totalShares);
+          feeBalance[investor] = feeBalance[investor].add(newFees);
         }
+
+        lastFeeDistribution = now;
     }
 }
